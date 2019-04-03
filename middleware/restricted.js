be900e82fd2da853c841f57b7f3e827f3c33f652
Brandon G-Home <brandon.gardner2@outlook.com>
2019-04-03 14:27:42 -0500

    token auth fully working including restricted middleware

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,8 +1,16 @@
+const checkToken = require("../auth/token-handlers").checkToken;
+
 module.exports = (req, res, next) => {
-  //change this
-  if (req.session && req.session.user) {
-    next();
+  const token = req.headers.authorization;
+  if (token) {
+    const decToken = checkToken(token);
+
+    if (decToken) {
+      next();
+    } else {
+      res.status(401).json({ message: "Invalid token." });
+    }
   } else {
-    res.status(400).json({ message: "Missing access credentials." });
+    res.status(400).json({ message: "No token provided." });
   }
 };
